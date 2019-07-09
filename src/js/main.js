9610ec83fb7d2404caa9550e6fa5508785a5ec3c
Amin Fazeli <aminfazeli787@yahoo.com>
2019-07-09 18:58:57 +0700

    add modal--full class

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,8 @@
-console.log('Write your hw 3 code here');
+const form = document.forms[0];
+const payButton = form.elements.submit;
+
+payButton.addEventListener('click', function(e) {
+  e.preventDefault();
+  form.classList.toggle('modal--full');
+  setTimeout('3000');
+});
